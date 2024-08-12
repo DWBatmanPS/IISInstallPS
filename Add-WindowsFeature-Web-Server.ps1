4ee50ee8c29c50d1a59750c12b0be99fcc2ab7d8
DWBatmanPS <96199880+DWBatmanPS@users.noreply.github.com>
2024-08-12 08:07:49 -0500

    Update Add-WindowsFeature-Web-Server.ps1
    adding comments

--- Add-WindowsFeature-Web-Server.ps1
+++ Add-WindowsFeature-Web-Server.ps1
@@ -1,8 +1,15 @@
+##Install IIS
 Add-WindowsFeature Web-Server
+
+##Loading powershell modules
 Import-Module -Name WebAdministration
+
+##Begin basic site configuration
 Remove-IISSite -Name "Default Web Site" -Confirm:$false
 New-IISSite -Name "LabSite" -BindingInformation "*:80:" -PhysicalPath "$env:systemdrive\inetpub\wwwroot"
 Set-Content -Path "$env:systemdrive\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) !"
+
+##Configure Logging
 $logdir = '%SystemDrive%\inetpub\logs\LogFiles'
 Set-ItemProperty `
     -Path 'IIS:\Sites\LabSite' `
@@ -11,4 +18,6 @@ Set-ItemProperty `
 Invoke-Expression "& '$env:WINDIR\system32\inetsrv\appcmd.exe' unlock config -section:system.applicationHost/log"
 Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.applicationHost/sites/site[@name='LabSite']/logFile/customFields" -name "." -value @{logFieldName='Content-Length';sourceName='Content-Length';sourceType='RequestHeader'}
 Set-WebConfigurationProperty "/system.applicationHost/sites/siteDefaults" -name logfile.directory -value $logdir
+
+##Lock IIS COnfig down.
 Invoke-Expression "& '$env:WINDIR\system32\inetsrv\appcmd.exe' lock config -section:system.applicationHost/log"
