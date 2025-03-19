7e42216b78f209d449ab660c569284f912a8a334
DWBatmanPS <96199880+DWBatmanPS@users.noreply.github.com>
2025-03-19 08:18:00 -0500

    Create Add-IIS-Custom-Logging.ps1

---
+++ Add-IIS-Custom-Logging.ps1
@@ -0,0 +1,27 @@
+##Install IIS
+Add-WindowsFeature Web-Server
+
+##Loading powershell modules
+Import-Module -Name WebAdministration
+
+##Begin basic site configuration
+Remove-IISSite -Name "Default Web Site" -Confirm:$false
+New-IISSite -Name "LabSite" -BindingInformation "*:80:" -PhysicalPath "$env:systemdrive\inetpub\wwwroot"
+Set-Content -Path "$env:systemdrive\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername) !"
+
+##Configure Logging
+$logdir = '%SystemDrive%\inetpub\logs\LogFiles'
+Set-ItemProperty `
+    -Path 'IIS:\Sites\LabSite' `
+    -Name Logfile.enabled `
+    -Value $true
+Invoke-Expression "& '$env:WINDIR\system32\inetsrv\appcmd.exe' unlock config -section:system.applicationHost/log"
+Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.applicationHost/sites/site[@name='LabSite']/logFile/customFields" -name "." -value @{logFieldName='Content-Length';sourceName='Content-Length';sourceType='RequestHeader'}
+Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.applicationHost/sites/site[@name='LabSite']/logFile/customFields" -name "." -value @{logFieldName='Host';sourceName='Host';sourceType='RequestHeader'}
+Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.applicationHost/sites/site[@name='LabSite']/logFile/customFields" -name "." -value @{logFieldName='User-Agent';sourceName='UserAgent';sourceType='RequestHeader'}
+Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.applicationHost/sites/site[@name='LabSite']/logFile/customFields" -name "." -value @{logFieldName='X-Forwarded-For';sourceName='X-Forwarded-For';sourceType='RequestHeader'}
+Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.applicationHost/sites/site[@name='LabSite']/logFile/customFields" -name "." -value @{logFieldName='X-Client-Cert';sourceName='X-Client-Cert';sourceType='RequestHeader'}
+Set-WebConfigurationProperty "/system.applicationHost/sites/siteDefaults" -name logfile.directory -value $logdir
+
+##Lock IIS COnfig down.
+Invoke-Expression "& '$env:WINDIR\system32\inetsrv\appcmd.exe' lock config -section:system.applicationHost/log"
